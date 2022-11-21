c8ee950cdd3f9a4c5f35f2bab47d67ce18d4332b
Juan Ap <juncaper0123@gmail.com>
2022-11-20 22:32:43 -0500

    imagen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import TextField from '@mui/material/TextField';
-import { red } from '@mui/material/colors';
 
 
 function App() {
@@ -32,78 +31,79 @@ function App() {
       setrederOne(false)
     }
     if ((ladoOne === ladotwo && ladoOne !== ladoThree) || (ladotwo === ladoThree && ladotwo !== ladoOne) || (ladoOne === ladoThree && ladoOne !== ladotwo)) {
-    setrenderTWo(true)
-  } else {
-    setrenderTWo(false)
+      setrenderTWo(true)
+    } else {
+      setrenderTWo(false)
+    }
+
+    if (angOne === angTwo && angOne === angThree && angTwo === angThree) {
+      setrenderFive(true)
+    } else {
+      setrenderFive(false)
+    }
+
+    if ((angOne === angTwo && angOne !== angThree) || (angTwo === angThree && angTwo !== angOne) || (angOne === angThree && angOne !== angTwo)) {
+      setrenderSix(true)
+    } else {
+      setrenderSix(false)
+    }
+    if (angOne !== angTwo && angOne !== angThree && angTwo !== angThree) {
+      setrenderSeven(true)
+    } else { setrenderSeven(false) }
+
+    if (angOne === 90 && angTwo !== 90 && angThree !== 90 || angOne !== 90 && angTwo === 90 && angThree !== 90 || angOne !== 90 && angTwo !== 90 && angThree === 90) {
+      setrenderEight(true)
+
+    } else {
+      setrenderEight(false)
+      if (ladoOne !== ladotwo && ladoOne !== ladoThree && ladotwo !== ladoThree) {
+        setrenderThree(true)
+      } else {
+        setrenderThree(false)
+      }
+    }
+
+
+  }, [ladoOne, ladotwo, ladoThree, angOne, angTwo, angThree]);
+
+
+
+  const Equilatero = () => {
+    return (
+      <div className='cards'>
+        <h2>
+          El triangulo ingresado es equilatero, debido a que todos sus lados son iguales.
+        </h2>
+        <img src='image1.png' alt='equilatero' width={600} />
+      </div>)
   }
-  
-  if (angOne === angTwo && angOne === angThree && angTwo === angThree) {
-    setrenderFive(true)
-  } else {
-    setrenderFive(false)
+  const Isoceles = () => {
+    return (
+      <div className='cards'>
+        <h2>
+          El triangulo ingresado es isoceles, debido a que posee dos lados iguales y uno diferente.
+        </h2>
+        <img src='/image3.png' alt='isoceles' width={600} />
+      </div>)
   }
-
-  if ((angOne === angTwo && angOne !== angThree) || (angTwo === angThree && angTwo !== angOne) || (angOne === angThree && angOne !== angTwo)) {
-    setrenderSix(true)
-  } else {
-    setrenderSix(false)
+  const Escaleno = () => {
+    return (
+      <div className='cards'>
+        <h2>
+          El triangulo ingresado es escaleno, debido a que todos sus lados son distintos.
+        </h2>
+        <img src='/image2.png' alt='escaleno' width={600} />
+      </div>)
   }
-  if (angOne !== angTwo && angOne !== angThree && angTwo !== angThree) {
-    setrenderSeven(true)
-  } else { setrenderSeven(false) }
-
-  if (angOne === 90&&angTwo!==90&&angThree!==90||angOne !== 90&&angTwo === 90 &&angThree!==90 || angOne !== 90&&angTwo !== 90 &&angThree === 90 ) {
-    setrenderEight(true)
-
-  } else { setrenderEight(false)
-    if (ladoOne !== ladotwo && ladoOne !== ladoThree && ladotwo !== ladoThree) {
-    setrenderThree(true)
-  } else {
-    setrenderThree(false)
+  const Rectangulo = () => {
+    return (
+      <div className='cards'>
+        <h2>
+          El triangulo ingresado es un triangulo rectangulo, debido a que uno de sus angulos internos es de 90 grados.
+        </h2>
+        <img src='/image4.png' alt='escaleno' width={600} />
+      </div>)
   }
- }
-
-
-  }, [ladoOne, ladotwo, ladoThree,angOne,angTwo,angThree]);
-
-  
-
-const Equilatero =()=>{
-  return(
-<div className='cards'>
-<h2>
-El triangulo ingresado es equilatero, debido a que todos sus lados son iguales.
-</h2>
-<img src='image1.png' alt='equilatero' width={600}/>  
-</div>)
-}
-const Isoceles =()=>{
-  return(
-<div className='cards'>
-  <h2>
-  El triangulo ingresado es isoceles, debido a que posee dos lados iguales y uno diferente.
- </h2>
-  <img src='/image3.png' alt='isoceles' width={600} />
- </div> )
-}
-const Escaleno =()=>{
-  return(
-<div className='cards'>
-  <h2>
-    El triangulo ingresado es escaleno, debido a que todos sus lados son distintos.
- </h2>
-      <img src='/image2.png' alt='escaleno' width={600}/>
- </div>  )
-}
-const Rectangulo =()=>{
-  return(
-<div className='cards'>
-  <h2>
-  El triangulo ingresado es un triangulo rectangulo, debido a que uno de sus angulos internos es de 90 grados.
- </h2>
-      <img src='/image4.png' alt='escaleno' width={600}/>
- </div>  )
-}
 
 
   return (
@@ -136,13 +136,13 @@ const Rectangulo =()=>{
               setladoThree(event.target.value)
             }}
           className='textTriLado' />
-      </div> 
-       { rederOne && Equilatero() }
-  {renderTWo && Isoceles()
- 
-  }
-  {renderThree&& Escaleno()
-  }
+      </div>
+      {rederOne && Equilatero()}
+      {renderTWo && Isoceles()
+
+      }
+      {renderThree && Escaleno()
+      }
 
       <h1>Hallar el tipo de triangulo por sus ángulos</h1>
       <div className='trianguloLados'>
@@ -169,10 +169,10 @@ const Rectangulo =()=>{
           className='textTriLado' />
       </div>
 
-    {renderFive&&Equilatero()}
-    {renderSix&& Isoceles()}
-        {renderEight&&Rectangulo() }
-      {renderSeven&& Escaleno()}
+      {renderFive && Equilatero()}
+      {renderSix && Isoceles()}
+      {renderEight && Rectangulo()}
+      {renderSeven && Escaleno()}
     </div>
   );
 
